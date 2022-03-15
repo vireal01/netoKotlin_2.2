62187e64a4bb780e285d96ab41892fa8513f67a7
vireal01 <vireal014@yandex.ru>
2022-03-15 18:36:52 +0400

    added jenkins file

---
+++ .jenkins/Jenkinsfile_test.groovy
@@ -0,0 +1,65 @@
+pipeline {
+    agent {
+        node {
+            label 'master'
+        }
+    }
+
+    options {
+        buildDiscarder logRotator( 
+                    daysToKeepStr: '1', 
+                    numToKeepStr: '10'
+            )
+    }
+    stages {
+        
+        stage('Cleanup Workspace') {
+            steps {
+                cleanWs()
+                sh """
+                echo "Cleaned Up Workspace For Project"
+                """
+            }
+        }
+
+        stage('Code Checkout') {
+            steps {
+                sh """
+                    echo "Code checkout"
+                    """
+            }
+        }
+
+        stage(' Unit Testing') {
+            steps {
+                sh """
+                echo "Running Unit Tests"
+                """
+            }
+        }
+
+        stage('Code Analysis') {
+            steps {
+                sh """
+                echo "Running Code Analysis"
+                """
+            }
+        }
+
+        stage('Build Deploy Code') {
+            when {
+                branch 'develop'
+            }
+            steps {
+                sh """
+                echo "Building Artifact"
+                """
+
+                sh """
+                echo "Deploying Code"
+                """
+            }
+        }
+
+    }   
+}
